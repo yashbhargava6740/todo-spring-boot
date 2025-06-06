918a784a26a5a83a86bdea66f971d6e0a516737a
ncoder <bhargavayash2015@gmail.com>
2025-06-06 23:11:07 +0530

    add: getter setter annotations

--- src/main/java/com/challengex/com/challengeapp/models/Challenge.java
+++ src/main/java/com/challengex/com/challengeapp/models/Challenge.java
@@ -1,5 +1,8 @@
 package com.challengex.com.challengeapp.models;
 
+import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonSetter;
+
 public class Challenge {
     private Long id;
     private String name;
@@ -13,30 +16,38 @@ public class Challenge {
         this.month = month;
     }
 
+    @JsonGetter("id")
     public Long getId() {
         return this.id;
     }
+    @JsonSetter("id")
     public void setId(Long id) {
         this.id = id;
     }
 
+    @JsonGetter("name")
     public String getName() {
         return this.name;
     }
+    @JsonSetter("name")
     public void setName(String name) {
         this.name = name;
     }
 
+    @JsonGetter("description")
     public String getDescription() {
         return this.description;
     }
+    @JsonSetter("description")
     public void setDescription(String description) {
         this.description = description;
     }
 
+    @JsonGetter("month")
     public String getMonth() {
         return this.month;
     }
+    @JsonSetter("month")
     public void setMonth(String month) {
         this.month = month;
     }
